1a7d7ae65603519c4c54a62c90e83e89b3e4a89d
ruslankorotkov <ruslankorotkov.rus@yandex.ru>
2022-12-01 18:01:40 +0300

    условный оператор. домашнее задание 3. задание 2.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        int ageBaby = 15;
-        if (ageBaby <= 5 )
+        int ageBaby = 7;
+        if (ageBaby < 5 )
         {System.out.println(" Если возраст ребёнка равен " + ageBaby + " он не может кататься на атракционе. ");}
         if (ageBaby >=5 && ageBaby <=14)
         {System.out.println(" Если возраст ребёнка равен " + ageBaby + " он может кататься только в сопровождении взрослого. Если взрослого нет, то кататься нельзя. ");}
-        if (ageBaby >= 14)
+        if (ageBaby > 14)
         {System.out.println(" Если ребенок старше " + ageBaby + " он может кататься без сопровождения взрослого. ");}
-
-
-
-
 
     }
 }
