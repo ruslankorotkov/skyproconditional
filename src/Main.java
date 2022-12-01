621eabcbb4ee9c27bc5d828fd876ba81968da848
ruslankorotkov <ruslankorotkov.rus@yandex.ru>
2022-12-01 12:14:37 +0300

    условный оператор.

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 17;
+        if (age >= 18) ;
+        {
+            System.out.println(" поздравляем пользователя с совершеннолетием ");
+        }
+        if (age < 18) ;
+        {
+            System.out.println(" возраст совершеннолетия ещё не наступил, нужно немного подождать ");
+        }
+
+    }
+}
