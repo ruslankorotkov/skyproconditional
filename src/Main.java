f199b33b2806fe6bbed96302ecb82ac86a7746f3
ruslankorotkov <ruslankorotkov.rus@yandex.ru>
2022-12-01 15:06:14 +0300

    условный оператор. домашнее задание 2. задание 1-2.

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,13 @@ public class Main {
         {System.out.println(" поздровляем с совершеннолетием. ");}
         else
         { System.out.println(" возраст совершеннолетия ещё не наступил, нужно немного подождать. ");}
+        int ageBaby = 25;
+        if (ageBaby >= 7)
+        { System.out.println(" ребёнок ходит в школу. ");} else { System.out.println("  ");}
+        if (ageBaby >= 18)
+        { System.out.println(" человек уже закончил школу и может отправляться в университет. ");} else { System.out.println("  ");}
+        if (ageBaby >= 24)
+        { System.out.println(" человек окончил университет и ему пора искать первую работу. ");} else { System.out.println("  ");}
 
     }
 }
