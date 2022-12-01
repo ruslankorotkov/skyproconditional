9844b2316aa62af3d655295e5dcb9389ef00270b
ruslankorotkov <ruslankorotkov.rus@yandex.ru>
2022-12-01 17:24:45 +0300

    условный оператор. домашнее задание 3. задание 1.

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        int age = 17;
-        if (age >= 18)
-        {System.out.println(" поздровляем с совершеннолетием. ");}
-        else
-        { System.out.println(" возраст совершеннолетия ещё не наступил, нужно немного подождать. ");}
-        int ageBaby = 25;
-        if (ageBaby >= 7)
-        { System.out.println(" ребёнок ходит в школу. ");} else { System.out.println("  ");}
-        if (ageBaby >= 18)
-        { System.out.println(" человек уже закончил школу и может отправляться в университет. ");} else { System.out.println("  ");}
-        if (ageBaby >= 24)
-        { System.out.println(" человек окончил университет и ему пора искать первую работу. ");} else { System.out.println("  ");}
-        int placeAll = 102;
-        int playSit = 60;
-        int playStay = 42;
-        if (  placeAll>= 102)
-        { System.out.println(" вагон забит. ");} else { System.out.println(" есть места. ");}
-        if ( playSit >= 60)
-        { System.out.println(" сидячих мест нет. ");} else { System.out.println(" есть сидячие места. ");}
-        if (playStay >= 42)
-        { System.out.println( " стоячих мест нет. ");} else { System.out.println(" есть стоячие места. ");}
+        int age = 44;
+        if (age >= 2 && age <=6)
+        {System.out.println(" Если возраст человека равен " + age + " то ему нужно ходить в детский сад. ");}
+        if (age >= 7 && age <=18)
+        {System.out.println(" Если возраст человека равен " + age + " то ему нужно ходить в школу. ");}
+        if (age >= 18 && age <=24)
+        {System.out.println( " Если возраст человека равен " + age +" то его место в университете. ");}
+        if (age >= 24)
+        {System.out.println(" Если возраст человека равен " + age + "то ему пора ходить на работу. ");}
+
+
 
     }
 }
