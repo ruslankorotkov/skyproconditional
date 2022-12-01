519000dbf14fad68c27642070cf04b3ddff064ae
ruslankorotkov <ruslankorotkov.rus@yandex.ru>
2022-12-01 14:35:58 +0300

    условный оператор. задание 4. задача 1-2-3.

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int age = 17;
         if (age >= 18);
-
         {System.out.println(" поздровляем с совершеннолетием. ");}
-
         if (age < 18);
-
         { System.out.println(" возраст совершеннолетия ещё не наступил, нужно немного подождать. ");}
         int babyAge = 10;
         if (babyAge >= 7);
