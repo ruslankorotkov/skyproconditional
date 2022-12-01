5a5f42d08d150f94c885fb0511d69887b54dc101
ruslankorotkov <ruslankorotkov.rus@yandex.ru>
2022-12-01 12:39:05 +0300

    условный оператор.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         int age = 17;
+        if (age >= 7) ;
+        {
+            System.out.println(" ребенок ходит в школу ");
+        }
         if (age >= 18) ;
         {
-            System.out.println(" поздравляем пользователя с совершеннолетием ");
+            System.out.println(" человек уже закончил школу и может отправляться в университет ");
         }
-        if (age < 18) ;
+        if (age >= 24) ;
         {
-            System.out.println(" возраст совершеннолетия ещё не наступил, нужно немного подождать ");
+            System.out.println(" человек окончил университет и ему пора искать первую работу ");
         }
 
     }
