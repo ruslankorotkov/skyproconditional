6a5878c482ee4425aac2880246ea90fd668eed46
ruslankorotkov <ruslankorotkov.rus@yandex.ru>
2022-12-03 09:47:40 +0300

    Условный оператор. Часть 2. Задание 4. (исправление)

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         int deliveryDistance = 95;
-        if ( deliveryDistance > 60 ) {
+        if (deliveryDistance <= 20) {
+            System.out.println(" Потребуется дней: 1 ");
+        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            System.out.println(" Потребуется дней: 2 ");
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
             System.out.println(" Потребуется дней: 3 ");
-        }else if ( deliveryDistance <= 20  ) {System.out.println( " Потребуется дней: 1 " );
-        }else if ( deliveryDistance <= 60 ) {System.out.println( " Потребуется дней: 2 " );
-        }else if ( deliveryDistance <= 100  ) {System.out.println( " Потребуется дней: 3 " );
+        } else {
+            System.out.println(" Потребуется дней: - ");
         }
     }
 }
