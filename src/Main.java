900b9bf9a6ea63931230c5e2c0d2d976fa0cfe26
ruslankorotkov <ruslankorotkov.rus@yandex.ru>
2022-12-03 11:27:25 +0300

    Условный оператор. Часть 2. Задание 3.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int deliveryDistance = 95;
-        if (deliveryDistance <= 20) {
-            System.out.println(" Потребуется дней: 1 ");
-        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
-            System.out.println(" Потребуется дней: 2 ");
-        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
-            System.out.println(" Потребуется дней: 3 ");
+        int year = 2021;
+        if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println( year + " год является високосным. ");
         } else {
-            System.out.println(" Потребуется дней: - ");
-        }
-    }
+            System.out.println(year + " год не является високосным. ");
+        }}
 }
 
 
