a6e91af8077d8520722849747e41190b46f76da6
ruslankorotkov <ruslankorotkov.rus@yandex.ru>
2022-12-04 23:26:53 +0300

    Условный оператор. Часть 2. Задание 1-2.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        {
-            System.out.println(" ура ура ура  ");
+        int IOS = 0;
+        int Android = 1;
+        int clientOS = 0;
+        int clientDeviceYear = 2016;
+        if (clientDeviceYear >= 2015) {
+            if (clientOS < 1) {
+                System.out.println(" Установите версию приложения для iOS по ссылке ");
+            } else {
+                System.out.println(" Установите  версию приложения для Android по ссылке ");
+            }
+        } else {
+            if
+            (clientOS > 0) {
+                System.out.println(" Установите  облегчённую версию приложения для Android по ссылке ");
+            } else {
+                System.out.println(" Установите облегченную версию приложения для iOS по ссылке ");
+            }
         }
     }
 }
