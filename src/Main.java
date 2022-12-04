e20a15b08b492413b867824be932d84642b0d317
ruslankorotkov <ruslankorotkov.rus@yandex.ru>
2022-12-04 10:10:17 +0300

    я прошёл тестирование!!!

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int year = 2021;
-        if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
-            System.out.println( year + " год является високосным. ");
-        } else {
-            System.out.println(year + " год не является високосным. ");
-        }}
+         {
+            System.out.println("я прошёл тестирование!!! ");
+      }}
 }
-
-
