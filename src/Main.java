d05bcb5caefe55cd4f162443420818434770a23c
ruslankorotkov <ruslankorotkov.rus@yandex.ru>
2022-12-06 18:37:15 +0300

    Условный оператор. Часть 2. Задание 1-2.маленькие изменения

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,16 @@ public class Main {
         int IOS = 0;
         int Android = 1;
         int clientOS = 0;
-        int clientDeviceYear = 2016;
-        if (clientDeviceYear >= 2015) {
-            if (clientOS < 1) {
-                System.out.println(" Установите версию приложения для iOS по ссылке ");
-            } else {
-                System.out.println(" Установите  версию приложения для Android по ссылке ");
-            }
-        } else {
-            if
-            (clientOS > 0) {
-                System.out.println(" Установите  облегчённую версию приложения для Android по ссылке ");
-            } else {
-                System.out.println(" Установите облегченную версию приложения для iOS по ссылке ");
-            }
-        }
+        int clientDeviceYear = 2013;
+        if ( clientDeviceYear >= 2015 )
+        { if ( clientOS < 1  )
+        {System.out.println( " Установите версию приложения для iOS по ссылке ");} else
+        {System.out.println( " Установите версию приложения для Android по ссылке ");}}
+        if ( clientDeviceYear < 2015 )
+        { if ( clientOS < 1  )
+        {System.out.println( " Установите облегченную версию приложения для iOS по ссылке ");} else
+        {System.out.println( " Установите облегченную версию приложения для Android по ссылке ");}}
+
     }
 }
+
